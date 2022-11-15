7e4f1ccf15a2c0f4e5595009acdb7d171916fdae
Sabine.Eitel <Seit201@aol.com>
2022-11-15 10:23:33 +0100

    Array

--- script_01.js
+++ script_01.js
@@ -2,12 +2,29 @@
 
 /* Die Satzbau-Maschine | Arrays */
 
+/*** 02. Theorie: Array ****/ 
+
+
+let arr;
+// arr = new Array(); // Konstruktor
+
+arr = []; // Literal
+arr = [2,7,11,4];  // 4 Elemente
+arr = [false,true,false];
+arr = ["ich","bin","Max","Mütze"];
+
+
+output(arr);
+output(arr.length)  // Anzahl der Elemente
+output(arr[0]);  // Index 0 (1. Pos. im Array)
+output(arr[3]); // konkret
+output(arr[arr.length-1]); // allgemein
 
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-output(getSentence("ich","bin","der","coole","Max","Mütze"));
-output(getSentence("ich","bin","die","coole","Maxine","Mützerich"));
+// output(getSentence("ich","bin","der","coole","Max","Mütze"));
+// output(getSentence("ich","bin","die","coole","Maxine","Mützerich"));
 // more of the same
 function getSentence(word1,word2,word3,word4,word5,word6) {
 
