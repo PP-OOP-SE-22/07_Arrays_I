67835982837511601f8ede784a1edd18be3efa2d
Sabine.Eitel <Seit201@aol.com>
2022-11-15 09:17:04 +0100

    test2

--- script_01.js
+++ script_01.js
@@ -6,15 +6,19 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-output(getSentence("ich","bin","Max","Mütze"));
-function getSentence(word1,word2,word3,word4) {
+output(getSentence("ich","bin","der","coole","Max","Mütze"));
+output(getSentence("ich","bin","die","coole","Maxine","Mützerich"));
+
+function getSentence(word1,word2,word3,word4,word5,word6) {
 
     const gab = " ";
     const dot = ".";
       let str =  word1 + gab +
                  word2 + gab +
                  word3 + gab +
-                 word4 + dot;
+                 word4 + gab +
+                 word5 + gab +
+                 word6 + dot;
     
     return str;
 }
